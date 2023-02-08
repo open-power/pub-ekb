bf607001d6c644b8f4d9f9e40f9e47c6893d383b
SWATHIPS <swathips@in.ibm.com>
2023-02-08 06:08:01 -0500

    pibms_reg_dump for ODY!
    *Addressed comments
    * To fix compilation Fail
    * sbe local register dump for ODY
    
    Change-Id: Ife8cdf825d4b0fd2087526440a4479a2e99f39fa
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/145419
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: sreekanth reddy kadapala Kadapala <skadapal@in.ibm.com>
    Reviewed-by: Joachim Fenkes <fenkes@de.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>

---
+++ chips/ocmb/odyssey/procedures/hwp/utils/ody_pibms_reg_dump.C
@@ -0,0 +1,144 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: chips/ocmb/odyssey/procedures/hwp/utils/ody_pibms_reg_dump.C $ */
+/*                                                                        */
+/* OpenPOWER EKB Project                                                  */
+/*                                                                        */
+/* COPYRIGHT 2015,2024                                                    */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+/// @file ody_pibms_reg_dump.C
+/// @brief Dump PIB Masters and Slaves Internal Register contents
+//
+// Change History:
+// 02/08/2023:  Copied from p10_pibms_reg_dump, changed to use in ody
+//
+
+// -----------------------------------------------------------------------------
+//  Includes
+// -----------------------------------------------------------------------------
+#include <fapi2.H>
+#include <ody_pibms_regs2dump.H>
+#include <ody_pibms_reg_dump.H>
+#include "p10_scom_perv_2.H"
+#include "p10_scom_perv_7.H"
+#include "p10_scom_proc_0.H"
+#include "p10_scom_proc_4.H"
+#include <string>
+
+// -----------------------------------------------------------------------------
+//  Function definitions
+// -----------------------------------------------------------------------------
+
+/// @brief Dump the contents of PIB Masters and Slaves Internal Registers
+///
+/// @param [in]  i_target             Chip target
+///
+/// @retval FAPI_RC_SUCCESS
+/// @retval ERROR defined in xml
+
+fapi2::ReturnCode ody_pibms_reg_dump( const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target,
+                                      std::vector<sRegV>& regv_set)
+{
+    using namespace scomt;
+
+    fapi2::buffer<uint32_t> l_data32_cbs_cs;
+    fapi2::buffer<uint32_t> l_data32_sb_cs;
+    fapi2::buffer<uint32_t> l_data32;
+    fapi2::buffer<uint64_t> l_data64_sl_clk_status;
+    uint64_t addr;
+    bool sdb, pib_clks_not_running, net_clks_not_running;
+    std::string reg_name;
+
+    FAPI_INF("Executing ody_pibms_reg_dump");
+
+    fapi2::buffer<uint64_t> buf;
+
+    FAPI_TRY(fapi2::getCfamRegister(i_target, PERV_CBS_CS_FSI, l_data32_cbs_cs));  //0x2801
+    FAPI_TRY(fapi2::getCfamRegister(i_target, PERV_SB_CS_FSI, l_data32_sb_cs));    //0x2808
+
+    FAPI_DBG("Release PCB reset");
+    l_data32.flush<0>().setBit<perv::FSXCOMP_FSXLOG_ROOT_CTRL0_PCB_RESET_DC>();
+    FAPI_TRY(putCfamRegister(i_target, perv::FSXCOMP_FSXLOG_ROOT_CTRL0_CLEAR_FSI, l_data32 ));
+    // only dump registers can be accessed in secure debug mode
+    // PERV_CBS_CS_SECURE_ACCESS_BIT = 4,  PERV_SB_CS_SECURE_DEBUG_MODE = 0
+    sdb = (l_data32_cbs_cs.getBit<BIT04>() && l_data32_sb_cs.getBit<BIT00>());
+
+    if(l_data32_cbs_cs.getBit<BIT04>() && !l_data32_sb_cs.getBit<BIT00>())
+    {
+        FAPI_ERR("PIB Master Slave registers can not be accessed in secure mode without setting secure debug bit");
+        goto fapi_try_exit;
+    }
+
+    if(!l_data32_cbs_cs.getBit<BIT04>())
+    {
+
+        FAPI_TRY(getScom(i_target, proc::TP_TPCHIP_TPC_CLOCK_STAT_SL, l_data64_sl_clk_status));
+
+        pib_clks_not_running = l_data64_sl_clk_status.getBit<proc::TP_TPCHIP_TPC_CLOCK_STAT_SL_UNIT2_SL>();
+        net_clks_not_running = l_data64_sl_clk_status.getBit<proc::TP_TPCHIP_TPC_CLOCK_STAT_SL_UNIT4_SL>();
+    }
+    else
+    {
+        pib_clks_not_running = false;
+        net_clks_not_running = false;
+    }
+
+
+    if (sdb)
+    {
+        FAPI_INF("In secure debug mode");
+    }
+
+    if (regv_set.size())
+    {
+        for (std::vector<sRegV>::iterator itr = regv_set.begin(); itr != regv_set.end(); ++itr)
+        {
+            //only read registers which can be accessed in secure debug mode
+            //MBX registers are always accessed using FSI scope
+            if (!sdb || ((itr->reg).attr & SDB_ATTR) || ((itr->reg).attr & MBX_ATTR))
+            {
+                addr = (itr->reg).addr;
+                reg_name = (itr->reg).name;
+
+                if( ! (pib_clks_not_running && ((reg_name.find("PIBMEM") != std::string::npos)
+                                                || (reg_name.find("PSU") != std::string::npos))) &&
+                    ! (net_clks_not_running && (reg_name.find("PCBM") != std::string::npos)))
+                {
+                    if((itr->reg).attr & MBX_ATTR)
+                    {
+                        FAPI_TRY(fapi2::getCfamRegister(i_target, ((addr & 0xFFFF) | 0x2800), l_data32));
+                        itr->value = (0x00000000FFFFFFFFull & l_data32) << 32;
+                    }
+                    else
+                    {
+                        FAPI_TRY((getScom(i_target , addr , buf)));
+                        itr->value = buf;
+                    }
+
+                    //mark value is valid
+                    (itr->reg).attr |= VLD_ATTR;
+                }
+            }
+        }
+    }
+
+fapi_try_exit:
+    FAPI_INF("Exiting ody_pibms_reg_dump");
+    return fapi2::current_err;
+}
---
+++ chips/ocmb/odyssey/procedures/hwp/utils/ody_sbe_localreg_dump.C
@@ -0,0 +1,211 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: chips/ocmb/odyssey/procedures/hwp/utils/ody_sbe_localreg_dump.C $ */
+/*                                                                        */
+/* OpenPOWER EKB Project                                                  */
+/*                                                                        */
+/* COPYRIGHT 2015,2024                                                    */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+/// @file ody_sbe_localreg_dump.C
+/// @brief
+
+// *HWP HWP Owner       : Pradeep CN <pradeepcn@in.ibm.com>
+// *HWP Backup HWP Owner: Anay K Desai
+// *HWP FW Owner        :
+// *HWP Team            : PERV
+// *HWP Level           : 2
+// *HWP Consumed by     :
+
+///
+/// High-level procedure flow:
+/// @verbatim
+/// High-level procedure flow:
+///
+///
+/// @endverbatim
+
+// -----------------------------------------------------------------------------
+//  Includes
+// -----------------------------------------------------------------------------
+#include <fapi2.H>
+
+
+#include <ody_ppe_utils.H>
+#include <ody_sbe_localreg_dump.H>
+
+// ----------------------------------------------------------------------
+// Constants
+// ----------------------------------------------------------------------
+
+// these are probably in some include file
+
+
+enum SBE_LREGS
+{
+
+    SBE_MISC     = 0x2000 ,
+    SBE_EISR     = 0x0000 ,
+    SBE_EIMR     = 0x0020 ,
+    SBE_EIPR     = 0x0040 ,
+    SBE_EITR     = 0x0060 ,
+    SBE_EISTR    = 0x0080 ,
+    SBE_EINR     = 0x00A0 ,
+    SBE_TSEL     = 0x0100 ,
+    SBE_DBG      = 0x0120 ,
+    SBE_TBR      = 0x0140 ,
+    SBE_IVPR     = 0x0160 ,
+    SBE_LFR0     = 0x2040 ,
+    SBE_LFR1     = 0x2060
+
+
+};
+
+std::vector<SBEReg_t> v_sbe_local_regs =
+{
+
+    { SBE_MISC,      "SBE_MISC" },
+    { SBE_EISR,      "SBE_EISR" },
+    { SBE_EIMR,      "SBE_EIMR" },
+    { SBE_EIPR,      "SBE_EIPR" },
+    { SBE_EITR,      "SBE_EITR" },
+    { SBE_EISTR,     "SBE_EISTR" },
+    { SBE_EINR,      "SBE_EINR" },
+    { SBE_TSEL,      "SBE_TSEL" },
+    { SBE_DBG,       "SBE_DBG" },
+    { SBE_TBR,       "SBE_TBR" },
+    { SBE_IVPR,      "SBE_IVPR" },
+    { SBE_LFR0,      "SBE_LFR0" },
+    { SBE_LFR1,      "SBE_LFR1" },
+};
+
+
+
+// -----------------------------------------------------------------------------
+//  Function prototypes
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+//  Function definitions
+// -----------------------------------------------------------------------------
+
+/// @brief Dump the contents of the SBE SRAM
+///
+/// @param [in]  i_target                  Chip target
+/// @param [out] v_sbe_local_reg_value     Vector of structure of LocalReg
+///
+/// @retval FAPI_RC_SUCCESS
+/// @retval ERROR defined in xml
+
+fapi2::ReturnCode ody_sbe_localreg_dump( const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target ,
+        const uint16_t force_halt,
+        std::vector<SBESCOMRegValue_t>& v_sbe_local_reg_value)
+
+{
+
+    fapi2::buffer<uint64_t> l_data64;
+
+    SBESCOMRegValue_t l_regVal;
+
+    uint16_t address = 0 ;
+    uint32_t scom_address = 0 ;
+    uint64_t t_addr;
+
+    FAPI_INF("Executing ody_sbe_localreg_dump " );
+    fapi2::buffer<uint32_t> l_gpr0_save;
+    fapi2::buffer<uint32_t> l_gpr1_save;
+    fapi2::buffer<uint32_t> l_gpr9_save;
+    fapi2::buffer<uint64_t> l_sprg0_save;
+    fapi2::buffer<uint64_t> buf;
+
+    if (force_halt == 0)
+    {
+        FAPI_TRY(ppe_poll_halt_state(i_target , PPE_TYPE_SBE, 0));
+    }
+
+    else
+
+    {
+        FAPI_INF("ody_sbe_localreg_dump : Forcing halt thru SCOM");
+        FAPI_TRY(ppe_halt(i_target,  PPE_TYPE_SBE, 0));
+        FAPI_TRY(ppe_poll_halt_state(i_target, PPE_TYPE_SBE, 0));
+    }
+
+
+
+
+
+// If SBE is not in halt state
+    FAPI_INF("ody_sbe_localreg_dump : reading XIRAMDBG after polling");
+    t_addr = ppe_get_xir_address(PPE_TYPE_SBE, PPE_IDX_XIRAMDBG, 0);
+    FAPI_TRY(getScom(i_target, t_addr, buf),
+             "Error in GETSCOM");
+
+    if (!buf.getBit<0>())
+    {
+        FAPI_INF("ody_sbe_localreg_dump : Entering th unhalt state of SBE");
+
+        for (auto it : v_sbe_local_regs)
+        {
+            // ******************************************************************
+            scom_address = 0xC0000000 + it.number ; // defined in enums in p10_ppe_common.H files
+            FAPI_TRY(getScom(i_target , scom_address , buf ));
+
+            //  FAPI_INF(" %-6s : Local_reg_addr :  0xC000_%04x :  0x%016lX \n",it.name.c_str() , it.number , buf);
+            l_regVal.reg = it;
+            l_regVal.value = buf;
+            v_sbe_local_reg_value.push_back(l_regVal);
+            // ******************************************************************
+        }
+
+        //  FAPI_TRY(restore_gprs_sprs(i_target , l_gpr0_save, l_gpr1_save, l_gpr9_save, l_sprg0_save ));
+        FAPI_INF("< ody_sbe_localreg_dump");
+
+    }
+
+    // IF SBE is in halt state
+
+    else
+    {
+
+        //FAPI_INF("p10_sbe_localreg_dump : calling backup_gprs_sprs()...");
+
+        FAPI_TRY(ppe_save_gprs_sprs(i_target , PPE_TYPE_SBE, 0, l_gpr0_save, l_gpr1_save, l_gpr9_save, l_sprg0_save ));
+        FAPI_INF("ody_sbe_localreg_dump : Entering the HALT state of SBE");
+
+        for (auto it : v_sbe_local_regs)
+        {
+            // ******************************************************************
+            address = it.number ; // defined in enums in p10_ppe_common.H files
+            FAPI_TRY(ppe_local_reg_read(i_target , PPE_TYPE_SBE, 0, address , buf ));
+
+            //  FAPI_INF(" %-6s : Local_reg_addr :  0xC000_%04x :  0x%016lX \n",it.name.c_str() , it.number , buf);
+            l_regVal.reg = it;
+            l_regVal.value = buf;
+            v_sbe_local_reg_value.push_back(l_regVal);
+            // ******************************************************************
+        }
+
+        FAPI_TRY(ppe_restore_gprs_sprs(i_target , PPE_TYPE_SBE, 0, l_gpr0_save, l_gpr1_save, l_gpr9_save, l_sprg0_save ));
+        FAPI_INF("< ody_sbe_localreg_dump");
+    }
+
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
